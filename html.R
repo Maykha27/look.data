cef09cda3876f3953a101f0ce93a91d5cb078d28
Maykha27 <5144@holbertonschool.com>
2023-02-20 18:06:38 +0100

    html.R

---
+++ html.R
@@ -0,0 +1,27 @@
+    tags$head(tags$style(HTML('
+        /* logo */
+        .skin-blue .main-header .logo {
+                              background-color: #660099;
+                              }
+
+        /* navbar (rest of the header) */
+        .skin-blue .main-header .navbar {
+                              background-color: #660099;
+                              }
+
+        /* main sidebar */
+        .skin-blue .main-sidebar {
+                              background-color: #660099;
+                              }
+
+        /* active selected tab in the sidebarmenu */
+        .skin-blue .main-sidebar .sidebar .sidebar-menu .active a{
+                              background-color: #97DFC6;
+                              }
+
+        /* other links in the sidebarmenu */
+        .skin-blue .main-sidebar .sidebar .sidebar-menu a{
+                              background-color: #97DFC6;
+                              color: #000000;
+                              }
+                              '))),
